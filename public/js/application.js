6bdeff9eff98e08a2a511ba1b16b8e426fa0ff61
dbcbrianchung <brianjaychung@gmail.com>
2016-06-06 15:57:37 -0700

    add animation to marker drop

--- public/js/application.js
+++ public/js/application.js
@@ -19,40 +19,6 @@ function initMap() {
 
 $(document).ready(function(){
 
-	$("#user-submit-form").on("submit", function(event){
-
-		event.preventDefault();
-
-		$.ajax({
-			url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson',
-			type: 'GET'
-		}).done(function(data) {
-
-			console.log(data);
-			var earthquakePositions = [];
-			var earthquakes = data.features
-			earthquakes.forEach(function(earthquake) {
-				earthquakePositions.push(earthquake.geometry.coordinates);
-			});
-			console.log(earthquakePositions);
-			var quake = earthquakePositions[1];
-			var quakeLng = quake[0];
-			var quakeLat = quake[1];
-			var myLatLng = {lat: quakeLat, lng: quakeLng};
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: myLatLng,
-				zoom: 5
-			});
-
-			marker = new google.maps.Marker({
-				position: myLatLng,
-				map: map,
-				title: 'Hello World!'
-			});
-
-		});
-
-	});
 
 	$("#most-recent").on("submit", function(event){
 
@@ -83,6 +49,7 @@ $(document).ready(function(){
 			marker = new google.maps.Marker({
 				position: myLatLng,
 				map: map,
+				animation: google.maps.Animation.DROP,
 				title: 'Hello World!'
 			});
 			$('#quake-info').empty();
@@ -129,6 +96,7 @@ $(document).ready(function(){
 			marker = new google.maps.Marker({
 				position: myLatLng,
 				map: map,
+				animation: google.maps.Animation.DROP,
 				title: 'Hello World!'
 			});
 			$('#quake-info').empty();
@@ -175,6 +143,7 @@ $(document).ready(function(){
 			marker = new google.maps.Marker({
 				position: myLatLng,
 				map: map,
+				animation: google.maps.Animation.DROP,
 				title: 'Hello World!'
 			});
 			$('#quake-info').empty();
@@ -221,6 +190,7 @@ $(document).ready(function(){
 			marker = new google.maps.Marker({
 				position: myLatLng,
 				map: map,
+				animation: google.maps.Animation.DROP,
 				title: 'Hello World!'
 			});
 			$('#quake-info').empty();
