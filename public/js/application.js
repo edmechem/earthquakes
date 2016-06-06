2f5eeb0f0997436681d59870a1c4be0235a54d16
dbcbrianchung <brianjaychung@gmail.com>
2016-06-06 12:22:53 -0700

    add earthquake search functionality, need to allow search criteria

--- public/js/application.js
+++ public/js/application.js
@@ -1,53 +1,56 @@
-$(document).ready(function(){
-
-$("#user-submit-form").on("submit", function(event){
-	event.preventDefault();
-	$.ajax({
-		url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson',
-		type: 'GET'
-	}).done(function(data) {
-
-		console.log(data);
-		var earthquakePositions = [];
-		var earthquakes = data.features
-		earthquakes.forEach(function(earthquake) {
-			earthquakePositions.push(earthquake.geometry.coordinates);
-		});
-		console.log(earthquakePositions);
-	})
-
-})
 var map;
+var myLatLng = {lat: 37.784580, lng: -122.397437};
+var marker;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		zoom: 8
+	});
+
+	marker = new google.maps.Marker({
+		position: myLatLng,
+		map: map,
+		title: 'Hello World!'
+	});
+
+
 }
 
+$(document).ready(function(){
 
+	$("#user-submit-form").on("submit", function(event){
 
+		event.preventDefault();
 
+		$.ajax({
+			url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson',
+			type: 'GET'
+		}).done(function(data) {
 
+			console.log(data);
+			var earthquakePositions = [];
+			var earthquakes = data.features
+			earthquakes.forEach(function(earthquake) {
+				earthquakePositions.push(earthquake.geometry.coordinates);
+			});
+			console.log(earthquakePositions);
+			var quake = earthquakePositions[1];
+			var quakeLng = quake[0];
+			var quakeLat = quake[1];
+			var myLatLng = {lat: quakeLat, lng: quakeLng};
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: myLatLng,
+				zoom: 5
+			});
 
+			marker = new google.maps.Marker({
+				position: myLatLng,
+				map: map,
+				title: 'Hello World!'
+			});
 
+		});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	});
 })
