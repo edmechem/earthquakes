91a65426fc21d1a937362a519fa22a87bc076768
Ben Goldstein <email": null>
2016-06-07 10:03:12 -0700

    refactor for .done callbacks

--- public/js/application.js
+++ public/js/application.js
@@ -17,152 +17,95 @@ function initMap() {
 
 }
 
-$(document).ready(function(){   
-        $("#most-recent").on("click", function(event){
+$(document).ready(function(){
+  $("#most-recent").on("click", function(event){
 
-        	event.preventDefault();
+   event.preventDefault();
 
-        	$.ajax({
-        		url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson',
-        		type: 'GET',
-        		context: this
-        	}).done(function(data) {
+   $.ajax({
+    url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson',
+    type: 'GET',
+    context: this
+  }).done(printQuake);
 
-
-        		var earthquakePositions = [];
-        		var earthquakes = data.features
-        		earthquakes.forEach(function(earthquake) {
-        			earthquakePositions.push(earthquake.geometry.coordinates);
-        		});
-        		var quake = earthquakes[0];
-        		var quakePosition = earthquakePositions[0];
-        		var quakeLng = quakePosition[0];
-        		var quakeLat = quakePosition[1];
-        		var myLatLng = {lat: quakeLat, lng: quakeLng};
+ });
+  var printQuake = function(data) {
 
 
-        		map = new google.maps.Map(document.getElementById('map'), {
-        			center: myLatLng,
-        			zoom: 5
-        		});
-
-        		marker = new google.maps.Marker({
-        			position: myLatLng,
-        			map: map,
-        			animation: google.maps.Animation.DROP,
-        			title: 'Hello World!'
-        		});
-        		var infoWindow = new google.maps.InfoWindow(), marker, i;
-        		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        			return function() {
-        				infoWindow.setContent("Location: "+quake.properties.place+"<br> Magnitude: "+quake.properties.mag);
-        				infoWindow.open(map, marker);
-        			}
-        		})(marker, i));
-        		$('.well').empty();
-        		$('.well').append(quake.properties.place);
-
-        	});
-
-        });
-
-        $("#biggest-month").on("click", function(event){
-
-        	event.preventDefault();
-		// var endTimeSeconds = Date.now();
-		// var endTimeDate = new Date(0);
-		// endTimeDate.setUTCSeconds(endTimeSeconds);
-		var endTime = new Date();
-		var startTime = new Date();
-		startTime.setDate(startTime.getDate()-30);
-		endTime = endTime.toISOString().substring(0,10);
-		startTime = startTime.toISOString().substring(0,10);
-
-		$.ajax({
-			url:"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&starttime="+startTime+"&endtime="+endTime,
-			type: 'GET',
-			context: this
-		}).done(function(data) {
+    var earthquakePositions = [];
+    var earthquakes = data.features
+    earthquakes.forEach(function(earthquake) {
+      earthquakePositions.push(earthquake.geometry.coordinates);
+    });
+    var quake = earthquakes[0];
+    var quakePosition = earthquakePositions[0];
+    var quakeLng = quakePosition[0];
+    var quakeLat = quakePosition[1];
+    var myLatLng = {lat: quakeLat, lng: quakeLng};
 
 
-			var earthquakePositions = [];
-			var earthquakes = data.features
-			earthquakes.forEach(function(earthquake) {
-				earthquakePositions.push(earthquake.geometry.coordinates);
-			});
-			var quake = earthquakes[0];
-			var quakePosition = earthquakePositions[0];
-			var quakeLng = quakePosition[0];
-			var quakeLat = quakePosition[1];
-			var myLatLng = {lat: quakeLat, lng: quakeLng};
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: myLatLng,
-				zoom: 5
-			});
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 5
+    });
 
-			marker = new google.maps.Marker({
-				position: myLatLng,
-				map: map,
-				animation: google.maps.Animation.DROP,
-				title: 'Hello World!'
-			});
-			$('.well').empty();
-			$('.well').append(quake.properties.place);
+    marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: 'Hello World!'
+    });
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infoWindow.setContent("Location: "+quake.properties.place+"<br> Magnitude: "+quake.properties.mag);
+        infoWindow.open(map, marker);
+      }
+    })(marker, i));
+    $('.well').empty();
+    $('.well').append(quake.properties.place);
 
-		});
+  }
 
-	});
+  $("#biggest-month").on("click", function(event){
 
-        $("#biggest-week").on("click", function(event){
+   event.preventDefault();
 
-        	event.preventDefault();
-		// var endTimeSeconds = Date.now();
-		// var endTimeDate = new Date(0);
-		// endTimeDate.setUTCSeconds(endTimeSeconds);
-		var endTime = new Date();
-		var startTime = new Date();
-		startTime.setDate(startTime.getDate()-7);
-		endTime = endTime.toISOString().substring(0,10);
-		startTime = startTime.toISOString().substring(0,10);
+   var endTime = new Date();
+   var startTime = new Date();
+   startTime.setDate(startTime.getDate()-30);
+   endTime = endTime.toISOString().substring(0,10);
+   startTime = startTime.toISOString().substring(0,10);
 
-		$.ajax({
-			url:"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&starttime="+startTime+"&endtime="+endTime,
-			type: 'GET',
-			context: this
-		}).done(function(data) {
+   $.ajax({
+     url:"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&starttime="+startTime+"&endtime="+endTime,
+     type: 'GET',
+     context: this
+   }).done(printQuake);
 
+ });
 
-			var earthquakePositions = [];
-			var earthquakes = data.features
-			earthquakes.forEach(function(earthquake) {
-				earthquakePositions.push(earthquake.geometry.coordinates);
-			});
-			var quake = earthquakes[0];
-			var quakePosition = earthquakePositions[0];
-			var quakeLng = quakePosition[0];
-			var quakeLat = quakePosition[1];
-			var myLatLng = {lat: quakeLat, lng: quakeLng};
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: myLatLng,
-				zoom: 5
-			});
+  $("#biggest-week").on("click", function(event){
 
-			marker = new google.maps.Marker({
-				position: myLatLng,
-				map: map,
-				animation: google.maps.Animation.DROP,
-				title: 'Hello World!'
-			});
-			$('.well').empty();
-			$('.well').append(quake.properties.place);
+   event.preventDefault();
 
-		});
+   var endTime = new Date();
+   var startTime = new Date();
+   startTime.setDate(startTime.getDate()-7);
+   endTime = endTime.toISOString().substring(0,10);
+   startTime = startTime.toISOString().substring(0,10);
 
-	});
+   $.ajax({
+     url:"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&starttime="+startTime+"&endtime="+endTime,
+     type: 'GET',
+     context: this
+   }).done(callback);
 
-        $("#biggest-day").on("click", function(event){
+ });
 
-        	event.preventDefault();
+  $("#biggest-day").on("click", function(event){
+
+   event.preventDefault();
 		// var endTimeSeconds = Date.now();
 		// var endTimeDate = new Date(0);
 		// endTimeDate.setUTCSeconds(endTimeSeconds);
@@ -176,90 +119,64 @@ $(document).ready(function(){
 			url:"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=magnitude&starttime="+startTime+"&endtime="+endTime,
 			type: 'GET',
 			context: this
-		}).done(function(data) {
-
-
-			var earthquakePositions = [];
-			var earthquakes = data.features
-			earthquakes.forEach(function(earthquake) {
-				earthquakePositions.push(earthquake.geometry.coordinates);
-			});
-			var quake = earthquakes[0];
-			var quakePosition = earthquakePositions[0];
-			var quakeLng = quakePosition[0];
-			var quakeLat = quakePosition[1];
-			var myLatLng = {lat: quakeLat, lng: quakeLng};
-			map = new google.maps.Map(document.getElementById('map'), {
-				center: myLatLng,
-				zoom: 5
-			});
-
-			marker = new google.maps.Marker({
-				position: myLatLng,
-				map: map,
-				animation: google.maps.Animation.DROP,
-				title: 'Hello World!'
-			});
-			$('.well').empty();
-			$('.well').append(quake.properties.place);
-
-		});
+		}).done(printQuake);
 
 	});
 
-        $("#all-day").on("click", function(event){
+  $("#all-day").on("click", function(event){
 
-        	event.preventDefault();
+   event.preventDefault();
 
-        	$.ajax({
-        		url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson',
-        		type: 'GET',
-        		context: this
-        	}).done(function(data) {
-
-
-        		var earthquakePositions = [];
-        		var earthquakes = data.features
-        		earthquakes.forEach(function(earthquake) {
-        			earthquakePositions.push(earthquake.geometry.coordinates);
-        		});
-        		map = new google.maps.Map(document.getElementById('map'), {
-        			center: myLatLng,
-        			zoom: 1
-        		});
-        		var bounds = new google.maps.LatLngBounds();
-        		$('.well').empty();
-
-        		var infoWindow = new google.maps.InfoWindow(), marker, i;
-
-        		for (i = 0; i < earthquakes.length; i++) {
-        			var quake = earthquakes[i];
-        			var quakePosition = earthquakePositions[i];
-        			var quakeLng = quakePosition[0];
-        			var quakeLat = quakePosition[1];
-        			var myLatLng = {lat: quakeLat, lng: quakeLng};
-        			var title = quake.properties.place;
-				// bounds.extend(myLatLng);
-				marker = new google.maps.Marker({
-					position: myLatLng,
-					map: map,
-					animation: google.maps.Animation.DROP,
-					title: title
-				}); 
-				google.maps.event.addListener(marker, 'click', (function(marker, i) {
-					return function() {
-						infoWindow.setContent("Location: "+earthquakes[i].properties.place+"<br> Magnitude: "+earthquakes[i].properties.mag);
-						infoWindow.open(map, marker);
-					}
-				})(marker, i));  
-							map.fitBounds(bounds);
-			};
+   $.ajax({
+    url:'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson',
+    type: 'GET',
+    context: this
+  }).done(printQuakes);
+ });
+  var printQuakes = function(data) {
 
 
-			map.setZoom(3);
-		});
+    var earthquakePositions = [];
+    var earthquakes = data.features
+    earthquakes.forEach(function(earthquake) {
+     earthquakePositions.push(earthquake.geometry.coordinates);
+   });
+    map = new google.maps.Map(document.getElementById('map'), {
+     center: myLatLng,
+     zoom: 1
+   });
+    var bounds = new google.maps.LatLngBounds();
+    $('.well').empty();
+
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    for (i = 0; i < earthquakes.length; i++) {
+     var quake = earthquakes[i];
+     var quakePosition = earthquakePositions[i];
+     var quakeLng = quakePosition[0];
+     var quakeLat = quakePosition[1];
+     var myLatLng = {lat: quakeLat, lng: quakeLng};
+     var title = quake.properties.place;
+        // bounds.extend(myLatLng);
+        marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: title
         });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infoWindow.setContent("Location: "+earthquakes[i].properties.place+"<br> Magnitude: "+earthquakes[i].properties.mag);
+            infoWindow.open(map, marker);
+          }
+        })(marker, i));
+        map.fitBounds(bounds);
+      };
+
+
+      map.setZoom(3);
+    }
 
 
 
-      })
+  })
